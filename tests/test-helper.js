161ab4883c5a43d6f9ffcaa6aec08cc37d75d78e
Ayushi-Sama <2068681@cognizant.com>
2022-07-26 19:05:45 +0530

    First Commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'hotel/app';
+import config from 'hotel/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
