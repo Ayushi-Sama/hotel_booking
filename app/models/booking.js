ecf68c293cc9eac41cee7b21d8eb30a552714fdb
Ayushi-Sama <2068681@cognizant.com>
2022-07-29 19:13:21 +0530

    Third Commit

--- app/models/booking.js
+++ app/models/booking.js
@@ -1,14 +1,23 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class BookingModel extends Model {
-    @attr title;
-    @attr owner;
-    @attr city;
-    @attr location;
-    @attr category;
-    @attr image;
-    @attr bedrooms;
-    @attr description;
+
+    @attr img;
+    @attr name;
+    @attr price;
+    @attr distance;
+    @attr date;
+    
+
+    // @attr title;
+    // @attr owner;
+    // @attr city;
+    // @attr location;
+    // @attr category;
+    // @attr image;
+    // @attr bedrooms;
+    // @attr description;
+    // @attr comments;
   
 
 }
